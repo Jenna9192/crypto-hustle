8aba1567481f79716d817814d805602119954456
Jenna9192 <jennali6611@gmail.com>
2025-10-22 18:42:58 -0700

    prices not showing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-
+import CoinInfo from "./components/CoinInfo"
 
 const API_KEY = import.meta.env.VITE_APP_API_KEY
 
@@ -10,15 +10,45 @@ function App() {
   useEffect(() => {
     const fetchAllCoinData = async () => {
       const response = await fetch(
-
+        "https://min-api.cryptocompare.com/data/top/totaltoptiervol?limit=10&assetClass=ALL&tsym=usd&api_key="
+        + API_KEY
       )
+      const json = await response.json()
+      setlist(json)
     }
-
+    fetchAllCoinData().catch(console.error)
   }, [])
-  return (
-    <>
 
-    </>
+  return (
+    <div className="whole-page">
+      <h1>My Crypto List</h1>
+      <ul>
+        {/* We want to check if our list exists
+            if it does, map the Data property of our list to
+            CoinInfo property of each entry*/}
+        {list?.Data
+          .map(data => data.CoinInfo)
+          .filter(coinData =>
+            coinData.Algorithm != "N/A" &&
+            coinData.ProofType != "N/A"
+          )
+
+          .map(coinData => (
+            <CoinInfo
+              image={coinData.ImageUrl}
+              name={coinData.FullName}
+              symbol={coinData.Name}
+            />
+          ))}
+        <input
+          type="text"
+          placeholder="Search..."
+          onChange={(inputString) => searchItems(inputString.target.value)}
+        />
+
+
+      </ul>
+    </div>
   )
 }
 
